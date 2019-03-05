954692bd82e08d0ba9271912777af868110a1f6a
Eugene Golub <zgolub@ukr.net>
2019-03-05 16:49:26 +0200

    wip

--- src/UnmodifiableList.java
+++ src/UnmodifiableList.java
@@ -1,2 +1,127 @@
-public class UnmodifiableList {
+import java.util.*;
+
+public class UnmodifiableList<E> implements List<E> {
+
+    private final List<E> unmodifiablePart;
+    private final List<E> modifiablePart;
+
+    public UnmodifiableList(List<E> unmodifiablePart, List<E> modifiablePart) {
+        this.unmodifiablePart = unmodifiablePart;
+        this.modifiablePart = modifiablePart;
+    }
+
+    @Override
+    public int size() {
+        return unmodifiablePart.size() + modifiablePart.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() != 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return unmodifiablePart.contains(o) || modifiablePart.contains(o);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return modifiablePart.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return modifiablePart.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
 }
