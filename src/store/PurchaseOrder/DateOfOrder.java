490f8d6d7fe801bb9f145c6236a0b4aa2d0681a3
Eugene Golub <zgolub@ukr.net>
2019-03-07 19:33:34 +0200

    Orders is bad

---
+++ src/store/PurchaseOrder/DateOfOrder.java
@@ -0,0 +1,57 @@
+package store.PurchaseOrder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateOfOrder implements Comparable<DateOfOrder> {
+    private Date orderDay;
+
+    public DateOfOrder() throws IOException, ParseException {
+        System.out.println("Введите дату в формате dd.MM.yyyy");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String date = reader.readLine();
+        SimpleDateFormat s = new SimpleDateFormat("dd.MM.yyyy");
+        Date y = s.parse(date);
+        System.out.println(s.format(y));
+    }
+
+    public Date getOrderDay() {
+        return orderDay;
+    }
+
+    @Override
+    public int compareTo(DateOfOrder o) {
+        Date firstDay = this.getOrderDay();
+        Date sacondDay = o.getOrderDay();
+        if (firstDay == null){
+            throw new NullPointerException();
+        }
+        return firstDay.compareTo(sacondDay);
+    }
+
+    public static void main(String[] args) throws NullPointerException{
+        DateOfOrder n = null;
+        DateOfOrder n2 = null;
+        try {
+            n = new DateOfOrder();
+        } catch (IOException e) {
+            System.out.println("Так не пойдет - IOException, бро");
+        } catch (ParseException e) {
+            System.out.println("Так не пойдет - ParseException, бро");
+        }
+        try {
+            n2 = new DateOfOrder();
+        } catch (IOException e) {
+            System.out.println("Так не пойдет - IOException, бро");
+        } catch (ParseException e) {
+            System.out.println("Так не пойдет - ParseException, бро");
+        }
+        n.compareTo(n2);
+
+    }
+
+}
---
+++ src/store/PurchaseOrder/OrderList.java
@@ -0,0 +1,4 @@
+package store.PurchaseOrder;
+
+public class OrderList {
+}
